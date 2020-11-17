1f6c91d0893abcb4ef619fd75bf2a107b3d41d76
Ateeb Mazhar <71271371+AteebMazhar17@users.noreply.github.com>
2020-11-17 12:46:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var database;
+var backgroundimg,canvas;
+var gameState = 0;
+var playerCount;
+var form,player,game;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+game = new Game();
+game.getState();
+game.start();
+
+ 
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
+
